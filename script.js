a8acfef7294a3f96a8024450a20974e30f3b711f
Lyubov <aira_dark@live.ru>
2019-05-12 00:46:52 +0300

    конкурс

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+class Car {
+  constructor() {
+    this.container = document.querySelector('.container-car');
+    this.categoryItem = document.querySelector('#category-cars');
+    this.categoryItem.addEventListener('change', (e) => {
+      this.container.innerHTML = '';
+      this.getCategory(e.target.value);
+    });
+  }
+
+  newCard(car) {
+    let card = document.createElement('div');
+    card.classList.add('item-card');
+    card.innerHTML = `<div class="img" style="background: url('${car.img}') no-repeat center; background-size: cover;"></div>
+                      <div class="about"><h3 class="name">${car.name}</h3>
+                      <p class="category">${this.upperCaseStr(car.category)}</p>
+                      <p class="description">${this.upperCaseStr(this.correction(car.description))}<span class="price">${car.price}$</span></p>
+                      <p class="descriptionAll">${this.upperCaseStr(car.description)}<span class="price">${car.price}$</span></p>
+                      </div>`;
+    this.container.appendChild(card);
+  }
+
+  getJSON() {
+
+    return fetch('file.json').then(response => response.json());
+
+  }
+
+  getCategory(category) {
+
+    this.getJSON().then(json => {
+      for (let i = 0; i < json.cars.length; i++) {
+        if (category == 'all') {
+
+          this.newCard(json.cars[i]);
+        } else if (category == json.cars[i].category) {
+          this.newCard(json.cars[i]);
+        }
+      }
+    });
+  }
+
+  upperCaseStr(str) {
+    str = str[0].toUpperCase() + str.slice(1);
+    return str;
+  }
+  
+  correction(str) {
+
+    (str.length > 135) ? (str = str.slice(0, 132) + '...') : str;
+    return str;
+  }
+}
+
+let car = new Car();
+car.getCategory('all');
